c96621ff8506f198a2675a5d6fddc504dea09d50
Dachafy <129044478+Dachafy@users.noreply.github.com>
2025-08-15 20:00:42 +0700

    Delete Week 2/Lessons directory

--- Week 2/Lessons/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-string fname = "John";
-    string lname = "Doe";
-
-    string fullname = fname + lname;
-
-    string fullname1 = fname + " " + lname;
-
-    cout << fullname << endl;
-    cout << fullname1 << endl;
